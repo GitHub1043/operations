eaf73e205208084af23ba33a25222c2ec21be941
AYDEN HOLGATE <AYDEN.HOLGATE@connect.7oaks.org>
2021-01-25 14:46:05 -0600

    Initial commit

---
+++ operations/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnSub = New System.Windows.Forms.Button()
+        Me.btnMult = New System.Windows.Forms.Button()
+        Me.btnDiv = New System.Windows.Forms.Button()
+        Me.btnIntDiv = New System.Windows.Forms.Button()
+        Me.btnPow = New System.Windows.Forms.Button()
+        Me.btnMod = New System.Windows.Forms.Button()
+        Me.txtNumber1 = New System.Windows.Forms.TextBox()
+        Me.txtNumber2 = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.lblTextResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(36, 126)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(104, 45)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnSub
+        '
+        Me.btnSub.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSub.Location = New System.Drawing.Point(36, 206)
+        Me.btnSub.Name = "btnSub"
+        Me.btnSub.Size = New System.Drawing.Size(104, 45)
+        Me.btnSub.TabIndex = 1
+        Me.btnSub.Text = "-"
+        Me.btnSub.UseVisualStyleBackColor = True
+        '
+        'btnMult
+        '
+        Me.btnMult.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMult.Location = New System.Drawing.Point(36, 286)
+        Me.btnMult.Name = "btnMult"
+        Me.btnMult.Size = New System.Drawing.Size(104, 45)
+        Me.btnMult.TabIndex = 2
+        Me.btnMult.Text = "˟"
+        Me.btnMult.UseVisualStyleBackColor = True
+        '
+        'btnDiv
+        '
+        Me.btnDiv.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDiv.Location = New System.Drawing.Point(36, 366)
+        Me.btnDiv.Name = "btnDiv"
+        Me.btnDiv.Size = New System.Drawing.Size(104, 45)
+        Me.btnDiv.TabIndex = 3
+        Me.btnDiv.Text = "÷"
+        Me.btnDiv.UseVisualStyleBackColor = True
+        '
+        'btnIntDiv
+        '
+        Me.btnIntDiv.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIntDiv.Location = New System.Drawing.Point(209, 126)
+        Me.btnIntDiv.Name = "btnIntDiv"
+        Me.btnIntDiv.Size = New System.Drawing.Size(104, 45)
+        Me.btnIntDiv.TabIndex = 4
+        Me.btnIntDiv.Text = "\"
+        Me.btnIntDiv.UseVisualStyleBackColor = True
+        '
+        'btnPow
+        '
+        Me.btnPow.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPow.Location = New System.Drawing.Point(209, 206)
+        Me.btnPow.Name = "btnPow"
+        Me.btnPow.Size = New System.Drawing.Size(104, 45)
+        Me.btnPow.TabIndex = 5
+        Me.btnPow.Text = "x²"
+        Me.btnPow.UseVisualStyleBackColor = True
+        '
+        'btnMod
+        '
+        Me.btnMod.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMod.Location = New System.Drawing.Point(209, 286)
+        Me.btnMod.Name = "btnMod"
+        Me.btnMod.Size = New System.Drawing.Size(104, 45)
+        Me.btnMod.TabIndex = 6
+        Me.btnMod.Text = "Mod"
+        Me.btnMod.UseVisualStyleBackColor = True
+        '
+        'txtNumber1
+        '
+        Me.txtNumber1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNumber1.Location = New System.Drawing.Point(63, 9)
+        Me.txtNumber1.Name = "txtNumber1"
+        Me.txtNumber1.Size = New System.Drawing.Size(100, 31)
+        Me.txtNumber1.TabIndex = 7
+        '
+        'txtNumber2
+        '
+        Me.txtNumber2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNumber2.Location = New System.Drawing.Point(185, 9)
+        Me.txtNumber2.Name = "txtNumber2"
+        Me.txtNumber2.Size = New System.Drawing.Size(100, 31)
+        Me.txtNumber2.TabIndex = 8
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.ForeColor = System.Drawing.Color.Blue
+        Me.lblResult.Location = New System.Drawing.Point(29, 61)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(36, 37)
+        Me.lblResult.TabIndex = 9
+        Me.lblResult.Text = "="
+        '
+        'lblTextResult
+        '
+        Me.lblTextResult.AutoSize = True
+        Me.lblTextResult.Location = New System.Drawing.Point(12, 9)
+        Me.lblTextResult.Name = "lblTextResult"
+        Me.lblTextResult.Size = New System.Drawing.Size(0, 13)
+        Me.lblTextResult.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(348, 449)
+        Me.Controls.Add(Me.lblTextResult)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtNumber2)
+        Me.Controls.Add(Me.txtNumber1)
+        Me.Controls.Add(Me.btnMod)
+        Me.Controls.Add(Me.btnPow)
+        Me.Controls.Add(Me.btnIntDiv)
+        Me.Controls.Add(Me.btnDiv)
+        Me.Controls.Add(Me.btnMult)
+        Me.Controls.Add(Me.btnSub)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnSub As System.Windows.Forms.Button
+    Friend WithEvents btnMult As System.Windows.Forms.Button
+    Friend WithEvents btnDiv As System.Windows.Forms.Button
+    Friend WithEvents btnIntDiv As System.Windows.Forms.Button
+    Friend WithEvents btnPow As System.Windows.Forms.Button
+    Friend WithEvents btnMod As System.Windows.Forms.Button
+    Friend WithEvents txtNumber1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNumber2 As System.Windows.Forms.TextBox
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents lblTextResult As System.Windows.Forms.Label
+
+End Class
---
+++ operations/Form1.vb
@@ -0,0 +1,150 @@
+﻿Public Class Form1
+
+    Private Sub txtNumber1_TextChanged(sender As Object, e As EventArgs) Handles txtNumber1.TextChanged
+        Me.lblTextResult.Text = ""
+    End Sub
+
+    Private Sub txtNumber2_TextChanged(sender As Object, e As EventArgs) Handles txtNumber2.TextChanged
+        Me.lblTextResult.Text = ""
+    End Sub
+
+    Private Sub lblResult_Click(sender As Object, e As EventArgs) Handles lblResult.Click
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Me.lblTextResult.Text = "Result"
+        '1) Declare variables
+        Dim intNum1 As Integer
+        Dim intNum2 As Integer
+        Dim intAnswer As Integer
+
+        '2) Set values
+        intNum1 = Val(txtNumber1.Text)
+        intNum2 = Val(txtNumber2.Text)
+
+        '3) Calculate
+        intAnswer = intNum1 + intNum2
+
+        '4) Display
+        lblResult.Text = "= " & intAnswer
+    End Sub
+
+    Private Sub btnSub_Click(sender As Object, e As EventArgs) Handles btnSub.Click
+        Me.lblTextResult.Text = "Result"
+        '1) Declare variables
+        Dim intNum1 As Integer
+        Dim intNum2 As Integer
+        Dim intAnswer As Integer
+
+        '2) Set values
+        intNum1 = Val(txtNumber1.Text)
+        intNum2 = Val(txtNumber2.Text)
+
+        '3) Calculate
+        intAnswer = intNum1 - intNum2
+
+        '4) Display
+        lblResult.Text = "= " & intAnswer
+
+    End Sub
+
+    Private Sub btnMult_Click(sender As Object, e As EventArgs) Handles btnMult.Click
+        Me.lblTextResult.Text = "Result"
+        '1) Declare variables
+        Dim intNum1 As Integer
+        Dim intNum2 As Integer
+        Dim intAnswer As Integer
+
+        '2) Set values
+        intNum1 = Val(txtNumber1.Text)
+        intNum2 = Val(txtNumber2.Text)
+
+        '3) Calculate
+        intAnswer = intNum1 * intNum2
+
+        '4) Display
+        lblResult.Text = "= " & intAnswer
+
+    End Sub
+
+    Private Sub btnDiv_Click(sender As Object, e As EventArgs) Handles btnDiv.Click
+        Me.lblTextResult.Text = "Result"
+        '1) Declare variables
+        Dim intNum1 As Integer
+        Dim intNum2 As Integer
+        Dim intAnswer As Integer
+
+        '2) Set values
+        intNum1 = Val(txtNumber1.Text)
+        intNum2 = Val(txtNumber2.Text)
+
+        '3) Calculate
+        intAnswer = intNum1 / intNum2
+
+        '4) Display
+        lblResult.Text = "= " & intAnswer
+
+    End Sub
+
+    Private Sub btnIntDiv_Click(sender As Object, e As EventArgs) Handles btnIntDiv.Click
+        Me.lblTextResult.Text = "Result"
+        '1) Declare variables
+        Dim intNum1 As Integer
+        Dim intNum2 As Integer
+        Dim intAnswer As Integer
+
+        '2) Set values
+        intNum1 = Val(txtNumber1.Text)
+        intNum2 = Val(txtNumber2.Text)
+
+        '3) Calculate
+        intAnswer = intNum1 \ intNum2
+
+        '4) Display
+        lblResult.Text = "= " & intAnswer
+
+    End Sub
+
+    Private Sub btnPow_Click(sender As Object, e As EventArgs) Handles btnPow.Click
+        Me.lblTextResult.Text = "Result"
+        '1) Declare variables
+        Dim intNum1 As Integer
+        Dim intNum2 As Integer
+        Dim intAnswer As Integer
+
+        '2) Set values
+        intNum1 = Val(txtNumber1.Text)
+        intNum2 = Val(txtNumber2.Text)
+
+        '3) Calculate
+        intAnswer = intNum1 ^ intNum2
+
+        '4) Display
+        lblResult.Text = "= " & intAnswer
+
+    End Sub
+
+    Private Sub btnMod_Click(sender As Object, e As EventArgs) Handles btnMod.Click
+        Me.lblTextResult.Text = "Result"
+        '1) Declare variables
+        Dim intNum1 As Integer
+        Dim intNum2 As Integer
+        Dim intAnswer As Integer
+
+        '2) Set values
+        intNum1 = Val(txtNumber1.Text)
+        intNum2 = Val(txtNumber2.Text)
+
+        '3) Calculate
+        intAnswer = intNum1 Mod intNum2
+
+        '4) Display
+        lblResult.Text = "= " & intAnswer
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
